6511654901abd18ad2764fd2e2a10a0a30d569c1
TonioW-hub <antoine.weiss@edu.ece.fr>
2025-11-21 10:54:12 +0100

    élec

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,9 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-
         //TOUTE LA PARTIE LOGIQUE
         GrapheVille grapheVille = new GrapheVille();
         HashMap<Integer, Habitation> listeHabitations = grapheVille.extraireHabitations("villes.txt");
         grapheVille.afficherHabitations();
-        
     }
 }
--- src/Model/GrapheVille.java
+++ src/Model/GrapheVille.java
@@ -7,10 +7,40 @@ public class GrapheVille {
     HashMap<Integer, Habitation> listeHabitations = new HashMap<>();
     HashMap<String, Rue> listeRues = new HashMap<>();
 
+    public String choixVille(String nomFichier) throws IOException {
+        HashSet<String> villes = new HashSet<>();
+        String ligne;
+        String ville;
+
+        FileReader lecteurFichier = new FileReader(".\\src\\" + nomFichier);
+        BufferedReader br = new BufferedReader(lecteurFichier);
+
+        while((ligne = br.readLine()) != null){
+            String[] tab = ligne.split(";");
+            villes.add(tab[7]);
+        }
+
+        System.out.println("Villes disponibles à l'étude : ");
+        for(String v : villes){
+            System.out.println(v);
+        }
+        System.out.println("Veuillez choisir une ville : ");
+        Scanner sc = new Scanner(System.in);
+        ville = sc.nextLine();
+        while(!villes.contains(ville)){
+            System.out.println("Ville inconnue retente ta chance : ");
+            ville = sc.nextLine();
+        }
+
+        return ville;
+    }
+
     public HashMap<Integer, Habitation> extraireHabitations(String nomFichier){
         // Première passe : créer toutes les habitations
         try (FileReader lecteurFichier = new FileReader(".\\src\\" + nomFichier);
              BufferedReader br = new BufferedReader(lecteurFichier)) {
+
+            String ville = choixVille(nomFichier);
 
             String ligne;
             int i = 0;
@@ -18,15 +48,10 @@ public class GrapheVille {
                 String[] tab = ligne.split(";");
 
                 int numeroMaison = Integer.parseInt(tab[2]);
-
                 boolean bis;
-                if (tab[3].equals("bis")){
-                    bis = true;
-                } else {
-                    bis = false;
-                }
+                bis = tab[3].equals("bis");
 
-                if(!listeHabitations.containsKey(numeroMaison)) {
+                if(tab[7].equals(ville)) {
                     Habitation h = new Habitation();
                     h.bis = bis;
                     h.numeroMaison = numeroMaison;
@@ -48,11 +73,14 @@ public class GrapheVille {
     }
 
     public void afficherHabitations(){
+        int i = 0;
         for(Habitation h : listeHabitations.values()){
             System.out.println("Habitation : " + h.nomDeLaRue);
             for(Arrete a : h.listeVoisins){
                 System.out.println("Voisin : " + a.arrivee);
             }
+            System.out.println("--------------------------------" + i);
+            i++;
         }
     }
 
@@ -69,6 +97,12 @@ public class GrapheVille {
                 Rue r = listeRues.get(tab[1]);
                 r.listeHabitation.add(h);
             }
+        }
+    }
+
+    public void trouverVoisin(){
+        for(Habitation h : listeHabitations.values()){
+
         }
     }
 }
